8ab6ebf7022aa8e0ce4319444d03d61433b9a94e
minaya-web <minaya@choixmedia.com>
2026-04-16 14:20:26 +0400

    feat: resource image webpack

---
+++ webpack-lesson-2/src/index.js
@@ -0,0 +1,7 @@
+import img from "./img.jpg";
+
+const image = document.createElement("img");
+image.src = img;
+image.width = 200;
+
+document.body.appendChild(image);
---
+++ webpack-lesson-2/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource"
+      }
+    ]
+  }
+};
